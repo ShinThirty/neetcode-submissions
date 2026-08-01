71edf10d3b0697ce4bc5554957e778e82bd74f08
Lingnan Liu <xmxu00@gmail.com>
2026-08-01 16:00:49 -0700

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def search(lo, hi, p):
+            while lo < hi:
+                mid = lo + (hi - lo) // 2
+                if p(mid):
+                    hi = mid
+                else:
+                    lo = mid + 1
+            
+            return lo
+        
+        predicate = lambda k: sum([(p + k - 1) // k for p in piles]) <= h
+
+        return search(1, max(piles), predicate)
